d8748cc3245cbd66e70f0cb96a73b57af3cc438b
Sergio Nibon <snibonjr@gmail.com>
2024-08-08 07:14:24 -0300

    atualização 08/08/2024

---
+++ aula-11/exe008.js
@@ -0,0 +1,7 @@
+var velocidade = 85
+
+console.log(`Sua velocidade é de ${velocidade}Km/h.`)
+if(velocidade > 60) { // condição simples
+    console.log(`Você ultrapassou a velocidade permitida, você será MULTADO!`)
+}
+console.log("Dirija sempre utilizando o cinto de segurança!")
