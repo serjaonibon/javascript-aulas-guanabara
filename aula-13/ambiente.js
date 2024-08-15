4e82ec0dd472c44bbfedad774359261b8ddf9e34
Sergio Nibon <snibonjr@gmail.com>
2024-08-14 22:25:04 -0300

    Atualização em 14/08/2024

---
+++ aula-13/ambiente.js
@@ -0,0 +1,37 @@
+var c = 1
+var pausa = c
+do {
+    console.log(`Imprima: ${c}`)
+    c++
+} while (c <= 7 ) 
+
+
+
+//while
+
+/* 
+
+var c = 1
+while (c <= 10) {
+    console.log(c)
+    c++
+}
+
+*/
+
+
+
+
+
+
+/**
+ * 
+ * 
+ * console.log("Tudo bem")
+console.log("Tudo bem")
+console.log("Tudo bem")
+console.log("Tudo bem")
+console.log("Tudo bem")
+console.log("Tudo bem")
+console.log("Tudo bem")
+ */
